f9c4b80baa180d06594e9c2cedb3d1cb2483bb41
Agha Jafari <nitro.naj@gmail.com>
2018-06-28 11:25:55 +1000

    adding jest, wrting first test  file

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,6 +1,28 @@
+const starWarsNames = require('../index');
+
+function isArrayOfStrings(array) {
+  return array.every(item => typeof item === 'string');
+}
+
+function isIncludedIn(array) {
+  return function itemIncluded(item) {
+    return array.includes(item);
+  };
+}
 
 describe('starwars-names', () => {
   test('should work', () => {
     expect(true).toBe(true);
+  });
+
+  test('should have a list of all vaiable names', () => {
+    // starWarsNames.all
+    const starWarsAllName = isArrayOfStrings(starWarsNames.all);
+    expect(starWarsAllName).toBeTruthy();
+  });
+
+  test('should allow me to get random name', () => {
+    // starWarsNames.random
+    expect(isIncludedIn(starWarsNames.all)(starWarsNames.random())).toBeTruthy();
   });
 });
